8a68911ed24a4b39f50505145947e4d44ecf0aae
Shawn DelPercio <sdelperc@gmail.com>
2020-07-10 12:47:55 -0400

    implemented ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,30 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-
+    # start cache of tickets (key = source, value = destination)
+    # start route list of size length
+    ticket_cache = {}
+    route = [None] * length
+    
+    # build cache
+    for ticket in tickets:
+        ticket_cache[ticket.source] = ticket.destination
+    
+    next_dest = "NONE"
+    for i in range(length):
+        if i == 0:
+            route[i] = ticket_cache[next_dest]
+            next_dest = ticket_cache[route[i]]
+            continue
+            
+        route[i] = next_dest
+        next_dest = ticket_cache[route[i]]
+        
     return route
+
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
+tickets = [ticket_1, ticket_2, ticket_3]
+
+print(reconstruct_trip(tickets, 3))
